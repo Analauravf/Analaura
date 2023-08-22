81505c0f72cd1b4e7b42bd20fef881429ced45e2
Analauravf <vianafantattoanalaura@gmail.com>
2023-08-22 11:35:03 -0300

    update

---
+++ 2208/exercício7.js
@@ -0,0 +1,9 @@
+let pai = 45
+let mãe = 44
+let filho = 13
+let soma = pai +mãe +filho;
+
+console.log("A idade do pai ",pai);
+console.log("A idade da mãe ",mãe);
+console.log("A idade do filho ",filho);
+console.log("A soma da idade ",soma);
