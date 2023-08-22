8e39fe37e6d532c116465165c63d99ca400b46c1
Analauravf <vianafantattoanalaura@gmail.com>
2023-08-22 09:02:01 -0300

    update

--- 2208/subtração.js
+++ 2208/subtração.js
@@ -5,3 +5,8 @@ let decimal1= 5.7;
 let decimal2= 2.3;
 subtração= decimal1-decimal2;
 console.log(subtração2); //Resultado:3.4
+
+let num1= 10;
+let num2= 3;
+let resultado= num1 - num2;
+console.log(resultado); //Resultado:7
