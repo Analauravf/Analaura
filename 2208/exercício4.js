409477bd447921a6bd6e53177767c1d39c689e83
Analauravf <vianafantattoanalaura@gmail.com>
2023-08-22 11:14:16 -0300

    update

---
+++ 2208/exercício4.js
@@ -0,0 +1,9 @@
+let salario =9.898;
+let aluguel =2.576;
+let contas =1.934;
+let extras =2.565;
+let saldo= salario- (aluguel + contas +extras);
+
+console.log("Seu salario mensal é", salario);
+console.log("voce paga" ,aluguel, "de aluguel",contas, "de contas e tem", extras, "de gastos extras");
+console.log("seu saldo disponivel é",saldo);
