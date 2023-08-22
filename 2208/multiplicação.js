9fd732b6f14b2b856227a6cd620c4262af6a7abd
Analauravf <vianafantattoanalaura@gmail.com>
2023-08-22 09:15:09 -0300

    update

--- 2208/multiplicação.js
+++ 2208/multiplicação.js
@@ -5,3 +5,6 @@ let decimal1= 2.5;
 let decimal= 1.5;
  multiplicação= decimal1 * decimal2;
 console.log(multiplicação2); //Resultado: 3.75
+
+let resultado= 10 * 0;
+console.log(resultado3); //Resultado:0
