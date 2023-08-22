986f7fd05f8b21617c57a53d5c9f106e117d5c42
Analauravf <vianafantattoanalaura@gmail.com>
2023-08-22 10:56:27 -0300

    updat

---
+++ 2208/exercício3.js
@@ -0,0 +1,7 @@
+let distância = 360;
+let velocidade = 120;
+let tempo = distância/velocidade;
+
+console.log("Distância:", distância);
+console.log("velocidade média:",velocidade);
+console.log("tempo gasto:",tempo);
