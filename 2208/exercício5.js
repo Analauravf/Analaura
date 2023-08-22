1e764250e63514f5dedb9e31566ae1dcebaf1175
Analauravf <vianafantattoanalaura@gmail.com>
2023-08-22 11:21:49 -0300

    update

--- 2208/exercício5.js
+++ 2208/exercício5.js
@@ -1,4 +1,9 @@
 let inicial= 50;
 let ganhou= 30;
 let perdeu= 15;
-let valor inicial + ganhou - perdeu
+let valor= inicial + ganhou - perdeu;
+
+console.log("pontuação inicial",inicial);
+console.log("pontos extras",ganhou);
+console.log("pontos perdidos",perdeu);
+console.log("pontuação final", valor);
