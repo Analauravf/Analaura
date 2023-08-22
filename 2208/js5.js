1b03e68f89d875887eb3f23390285eabd768ec16
Analauravf <vianafantattoanalaura@gmail.com>
2023-08-22 09:28:16 -0300

    update

--- 2208/js5.js
+++ 2208/js5.js
@@ -3,3 +3,6 @@ console.log(resultado1); //Resultado:20(soma primeiro,depois multiplicação)
 
 let resultado2= 10+5-2;
 console.log(resultado2); //Resultado:13(soma primeiro, depois subtração)
+
+let resultado3=(10+5) * 2-3
+console.log(resultado3); //Resultado:27(soma primeiro,depois multiplicaçõa e subtração)
