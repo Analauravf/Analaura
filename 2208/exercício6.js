e1ccc57846915f113f35090e1f14c5fa05e143a1
Analauravf <vianafantattoanalaura@gmail.com>
2023-08-22 11:27:52 -0300

    update

---
+++ 2208/exercício6.js
@@ -0,0 +1,9 @@
+let inicial =200;
+let adicionadas =150;
+let vendidas =250;
+let final =inicial + adicionadas - vendidas;
+
+console.log("Quantidade inicial",inicial);
+console.log("Quantidade adicionada",adicionadas);
+console.log("quantidade vendida",vendidas);
+console.log("quantidade final",final);
